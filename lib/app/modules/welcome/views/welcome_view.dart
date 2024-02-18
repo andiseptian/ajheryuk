22e75f73baaf96087f5d7381d29a269d4a1c5166
andiseptian <andiseptianadichandra@gmail.com>
2024-02-18 16:57:11 +0800

    halaman welcome selesai

--- lib/app/modules/welcome/views/welcome_view.dart
+++ lib/app/modules/welcome/views/welcome_view.dart
@@ -13,21 +13,56 @@ class WelcomeView extends GetView<WelcomeController> {
           title: const Text('WelcomeView'),
           centerTitle: true,
         ),
-        body: Column(
-          children: [
-            Container(
-              width: Get.width * 0.6,
-              height: Get.width * 0.6,
-              color: Colors.amber,
-              child: Image.asset(
-                "assets/logo/Logo.png",
-                fit: BoxFit.cover,
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: Get.width * 0.6,
+                height: Get.width * 0.6,
+                // color: Colors.amber,
+                child: Image.asset(
+                  "assets/logo/Logo.png",
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Text("Welcome to Ajheryuk"),
-            Text("Best and popular apps for live education course from home"),
-            ElevatedButton(onPressed: () {}, child: Text("Get Started"))
-          ],
+              SizedBox(
+                height: 100,
+              ),
+              Text(
+                "Welcome to Ajheryuk",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Best and popular apps for live education course from home",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+                selectionColor: Color(0xFF8C8C8C),
+              ),
+              SizedBox(
+                height: 75,
+              ),
+              Container(
+                  width: Get.width,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Get Started",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        primary: Color(0xffec5f5f),
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        shape: ContinuousRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20)))),
+                  )),
+            ],
+          ),
         ));
   }
 }
